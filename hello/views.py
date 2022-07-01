78be167340361ce606dd6dde6b572f051ccf0b03
Lee Dulcio <leedulcio@gorillac.net>
2022-07-01 17:54:39 -0400

    small corrections

--- hello/views.py
+++ hello/views.py
@@ -3,7 +3,7 @@ from django.utils.timezone import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def home(request):
+def home():
     return HttpResponse("Hello, Django!")
 
 def hello_there(request, name):
